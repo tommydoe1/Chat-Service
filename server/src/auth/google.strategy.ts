e9f14c475dc9227d79cb88d5213c611c31200e1f
tommydoe1 <tommyjdoe42@gmail.com>
2025-10-22 12:30:36 +0100

    Fix for google auth

--- server/src/auth/google.strategy.ts
+++ server/src/auth/google.strategy.ts
@@ -9,7 +9,7 @@ export const configureGoogleStrategy = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID ?? "",
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-        callbackURL: "/auth/google/callback",
+        callbackURL: process.env.GOOGLE_CALLBACK_URL ?? "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
